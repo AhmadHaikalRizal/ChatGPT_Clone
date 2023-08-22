3d9fa6c3f12efd09adb7fe702712009bbd6bd07c
Ahmad Haikal Rizal <131594378+AhmadHaikalRizal@users.noreply.github.com>
2023-08-22 12:32:42 +0700

    update script.js

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const sendButton = document.querySelector("#send-button");
 const sendButton = document.querySelector(".chat-container");
 const sendButton = document.querySelector("#send-button");
 const deleteButton = document.querySelector("#delete-button");
+
+let userText = null;
+const API_KEY = "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";
