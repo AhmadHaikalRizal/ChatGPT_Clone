d2f173a108a3bdedcb42a0a758878f8100b4ba45
Ahmad Haikal Rizal <131594378+AhmadHaikalRizal@users.noreply.github.com>
2023-08-22 12:31:55 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const chatInput = document.querySelector("#chat-input");
 const sendButton = document.querySelector("#send-button");
+const sendButton = document.querySelector(".chat-container");
+const sendButton = document.querySelector("#send-button");
+const deleteButton = document.querySelector("#delete-button");
